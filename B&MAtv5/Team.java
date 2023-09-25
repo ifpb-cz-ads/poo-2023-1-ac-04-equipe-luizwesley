b3c5c2ea5e57e49727d53202cb7e63e2d6e34c95
WescAlves <wesley.c.alves98@gmail.com>
2023-09-25 18:23:43 -0300

    Finalizando classe de Time

--- B&MAtv5/Team.java
+++ B&MAtv5/Team.java
@@ -1,7 +1,8 @@
 public class Team {
     private String nome;
     private String esporte;
-    private Jogador[] elenco;
+    private static Jogador[] elenco = new Jogador[10];
+    private static int contJogador = 0;
     public Team(String nome, String esporte, Jogador[] elenco) {
         this.nome = nome;
         this.esporte = esporte;
@@ -22,7 +23,31 @@ public class Team {
     public Jogador[] getElenco() {
         return elenco;
     }
-    public void setElenco(Jogador[] elenco) {
-        this.elenco = elenco;
+
+    public static void contrataJogador(String nome){
+        Jogador newPlayer = new Jogador(nome);
+        elenco[contJogador] = newPlayer;
+        contJogador++;
+        System.out.println("Jogador contratado.");
+    }
+    public static void demiteJogador(String nome){
+        for(int i=0; i<10; i++){
+            if(elenco[i].getNome() == nome){
+                elenco[i] = null;
+                i=100;
+                contJogador = contJogador - 1;
+                System.out.println("Jogador demitido.");
+            }
+            else{System.out.println("Não existe");}
+        }
+    }
+
+    public static void mostraJogadores(){
+        for(int j = 0; j<9; j++){
+            if(elenco[j] != null){
+
+                System.out.println(elenco[j].getNome());
+            } 
+        }
     }
 }
